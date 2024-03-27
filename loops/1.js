0aa2b07db19a6c82ede6fbef5818854cb4cbf79e
mikkelkaad <mikkelkaad@gmail.com>
2024-03-27 13:42:23 +0100

    Started working on loops

---
+++ loops/1.js
@@ -0,0 +1,8 @@
+//  1
+//  Use the for loop to console.log all the elements in the wallet array.
+
+let wallet = [10,15,27];
+
+for (i = 0; i < wallet.length; i++){
+    console.log('The wallet contains an element: ' + ?);
+};
